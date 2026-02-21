fe0b6c1850ff4825e4d8abb1ee3c32e5e9830278
OUISSAL EL HICHOU <114773271+ouissalhichou1@users.noreply.github.com>
2026-02-21 09:54:26 +0000

    init

---
+++ src/test/java/com/devops/bookshopapi/BookShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.bookshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
